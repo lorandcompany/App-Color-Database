b56111583b46e87e2f53fcc7b81f9543d1a9c952
Lor and Company <70607346+lorandcompany@users.noreply.github.com>
2021-11-21 20:28:04 +0800

    Update create-json.py

--- .github/workflows/create-json.py
+++ .github/workflows/create-json.py
@@ -1,18 +1,14 @@
 import os
 import re
 
-app_name_re = re.compile("App Name: (.*)")
-package_name_re = re.compile("Package Name: (.*)")
-hex_color_re = re.compile("Hex Color: (\#[.*]{6})")
-
 app_list = {}
 
 print(os.listdir("apps"))
 for item in os.listdir("apps"):
   with open(os.path.join("apps", item), "r") as file:
-    app_name = app_name_re.search(file.readline()).group(0)
-    package_name = package_name_re.search(file.readline()).group(0)
-    hex_color = hex_color_re.search(file.readline()).group(0)
+    app_name = re.search("App Name: (.*)", file.readline()).group(0)
+    package_name = re.search("Package Name: (.*)", file.readline()).group(0)
+    hex_color = re.search("Hex Color: (\#.{6})", file.readline()).group(0)
 
     print(app_name)
     print(package_name)
