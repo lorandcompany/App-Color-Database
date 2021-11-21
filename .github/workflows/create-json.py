d05fc1b4d8047a877d116250ee0419f5eaf895f4
Lor and Company <70607346+lorandcompany@users.noreply.github.com>
2021-11-21 20:08:25 +0800

    Update create-json.py

--- .github/workflows/create-json.py
+++ .github/workflows/create-json.py
@@ -8,11 +8,12 @@ hex_color_re = re.compile("Hex Color: (#.*{6})")
 app_list = {}
 
 print(os.listdir("/apps/"))
-for item in os.listdir("/apps/")
-  app_name = app_name_re.group(f.readline())
-  package_name = package_name_re.group(f.readline())
-  hex_color = hex_color_re.group(f.readline())
-  
-  print(app_name)
-  print(package_name)
-  print(hex_color)
+for item in os.listdir("/apps/"):
+  with f.open(item, "r") as file:
+    app_name = app_name_re.group(file.readline())
+    package_name = package_name_re.group(file.readline())
+    hex_color = hex_color_re.group(file.readline())
+
+    print(app_name)
+    print(package_name)
+    print(hex_color)
