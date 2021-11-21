0aad0c64c05169abb4192f2c73be1fc528babf66
helloimreallysadistic <70607346+lorandcompany@users.noreply.github.com>
2021-11-21 21:48:39 +0800

    Update create-json.py

--- .github/workflows/create-json.py
+++ .github/workflows/create-json.py
@@ -14,6 +14,11 @@ for item in os.listdir("apps"):
       app_name = re.search("App Name: (.*)", file.readline()).group(1)
       package_name = re.search("Package Name: (.*)", file.readline()).group(1)
       hex_color = re.search("Hex Color: (\#.{6})", file.readline()).group(1)
+
+      if package_name in packages:
+        print(f"[WARNING] File {item} with package name {package_name} already exists. Ignoring...")
+      packages.append(package_name)
+      app_list[package_name] = hex_color
     except Exception as e:
       invalid_files.append(item)
 
